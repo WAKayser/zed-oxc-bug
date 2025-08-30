ed7fde6bbcbf524a1e53a4acc6162a0a1073be70
Wouter Kayser <wouterkayser@gmail.com>
2025-08-30 12:31:40 +0200

    even more

---
+++ src/main<>.ts
@@ -0,0 +1,8 @@
+import { writeFile } from "node:fs/promises";
+
+const values = [1, 2, 3];
+for (const value of values) {
+  // angle brackets
+  await writeFile(`file${value}.txt`, ""); // line for oxlint
+  console.log("Completed"); // line for eslint
+}
